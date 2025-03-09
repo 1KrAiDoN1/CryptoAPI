ed85252d6a47e98def538086b6228cbe2c4f1d4c
1KrAiDoN1 <pavelvasilev24843@gmail.com>
2025-03-09 22:45:39 +0300

    Folder change

--- internal/service/registration.go
+++ internal/service/registration.go
@@ -1,10 +1,9 @@
-package database
+package service
 
 import (
 	"context"
 	"crypto/sha1"
 	"fmt"
-	"helloapp/internal/service"
 	"log"
 	"net/http"
 	"os"
@@ -53,10 +52,10 @@ func SendUserRegistrationData(w http.ResponseWriter, r *http.Request) {
 		}
 		http.Redirect(w, r, "/home", http.StatusSeeOther)
 		log.Printf("Пользователь с почтой %s зарегистрирован", email)
-		log.Printf("ID пользователя с почтой %s в базе данных: %d", email, service.GetUserIdFromDB(email, Password))
-		token, _ := service.GenerateJWToken(email, Password)
+		log.Printf("ID пользователя с почтой %s в базе данных: %d", email, GetUserIdFromDB(email, Password))
+		token, _ := GenerateJWToken(email, Password)
 		log.Print("token пользователя:", token)
-		id_from_token, _ := service.ParseToken(token)
+		id_from_token, _ := ParseToken(token)
 		log.Print("Полученный ID из токена: ", id_from_token)
 
 	}
